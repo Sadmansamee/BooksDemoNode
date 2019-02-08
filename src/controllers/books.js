fbea53876ff0b4457d1a35c1a48ad51aa05ffd82
sadman samee <sadman.tonmoy@gmail.com>
2019-02-08 09:02:23 +0600

    Update books.js

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -6,7 +6,7 @@ const _p             = require('../utils/promise_errors');
 const entryValidator = [check('url').isURL()]
 const {toPlain} = require('../utils/array_helper')
 
-router.get('/books',rejectInvalid,async (req,res,next)=>{
+router.get('/api/v1/books',entryValidator,rejectInvalid,async (req,res,next)=>{
 
     let [error,books] = await _p(Books.findAll({
         where:{
